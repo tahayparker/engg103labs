61b577e43c098e785e6bbd72b9334aad79ee0c7f
T Parker <98612931+tahayparker@users.noreply.github.com>
2023-11-16 13:25:07 +0400

    update

--- Lab 4/lab4.py
+++ Lab 4/lab4.py
@@ -5,90 +5,102 @@ import math
 from scipy.stats import linregress
 
 fig, ax = plt.subplots()
-
-def graph_common():
-    ax.set_facecolor('#EBEBEB')
-    ax.set_axisbelow(True)
-    [ax.spines[side].set_visible(False) for side in ax.spines]
-    ax.grid()
-    ax.grid(which='major', color='white', linewidth=1.2)
-    ax.grid(which='minor', color='white', linewidth=0.6)
-    ax.minorticks_on()
+ax.set_facecolor('#EBEBEB')
+ax.set_axisbelow(True)
+[ax.spines[side].set_visible(False) for side in ax.spines]
+ax.grid()
+ax.grid(which='major', color='white', linewidth=1.2)
+ax.grid(which='minor', color='white', linewidth=0.6)
+ax.minorticks_on()
 
 def metals():
+    # Import CSV for each metal
     s1 = np.genfromtxt("s1.csv", delimiter=",", autostrip=True)
     s2 = np.genfromtxt("s2.csv", delimiter=",", autostrip=True)
     s3 = np.genfromtxt("s3.csv", delimiter=",", autostrip=True)
 
+    # Plot each metal
     ax.plot(s1[0], s1[1], color='#e63946', linewidth=1)
     ax.plot(s2[0], s2[1], color='#2a9d8f', linewidth=1)
     ax.plot(s3[0], s3[1], color='#fb8500', linewidth=1)
     
+    # Name each metal
     m1 = "High Carbon Steel"
     m2 = "Galvanized Mild Steel"
     m3 = "Aluminum"
 
     stress_unit = "GPa"
 
+    # Set axis titles, graph title, and legend
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
     ax.set_title("Stresss vs Strain - Metals", fontweight ='bold')
     ax.legend([m1, m2, m3], loc='best')
 
 def pla():
+    # Import CSV for each material
     s4 = np.genfromtxt("s4.csv", delimiter=",", autostrip=True)
     s5 = np.genfromtxt("s5.csv", delimiter=",", autostrip=True)
     s6 = np.genfromtxt("s6.csv", delimiter=",", autostrip=True)
     
+    # Plot each material
     ax.plot(s4[0], s4[1], color='#593F62', linewidth=1)
     ax.plot(s5[0], s5[1], color='#005f73', linewidth=1)
     ax.plot(s6[0], s6[1], color='#8FC93A', linewidth=1)
     
+    # Name each material
     m1 = "PLA - Vertical Longitudinal Lines"
     m2 = "PLA - Horizontal Longitudinal Lines"
     m3 = "PLA - Mixed Orientations"
 
     stress_unit = "GPa"
 
+    # Set axis titles, graph title, and legend
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
     ax.set_title("Stresss vs Strain - PLA Samples", fontweight ='bold')
     ax.legend([m1, m2, m3], loc='best')
 
 def metal_elastic():
+    # Import CSV for each metal, only using elastic region
     s1 = np.genfromtxt("s1.csv", delimiter=",", autostrip=True, usecols=range(1,312))
     s2 = np.genfromtxt("s2.csv", delimiter=",", autostrip=True, usecols=range(1,378))
     s3 = np.genfromtxt("s3.csv", delimiter=",", autostrip=True, usecols=range(1,344))
 
+    # Assign x and y values for each metal
     x1, y1 = s1[0], s1[1]
     x2, y2 = s2[0], s2[1]
     x3, y3 = s3[0], s3[1]
     
+    # Plot each metal
     ax.plot(x1, y1, color='#e63946', linewidth=1)
     ax.plot(x2, y2, color='#2a9d8f', linewidth=1)
     ax.plot(x3, y3, color='#fb8500', linewidth=1)
 
+    # Print slope of each line for use in table
     print(linregress(x1, y1).slope)
     print(linregress(x2, y2).slope)
     print(linregress(x3, y3).slope)
 
-    # Get slope of each line using linregress
-    
+    # Get slope of each line using linregress for use in graph
     z1 = (linregress(x1, y1).slope) / 1000
     z2 = (linregress(x2, y2).slope) / 1000
     z3 = (linregress(x3, y3).slope) / 1000
 
+    # Name each metal with integrated slope
     m1 = "High Carbon Steel - Slope = {} GPa".format(z1.round(2))
     m2 = "Galvanized Mild Steel - Slope = {} GPa".format(z2.round(2))
     m3 = "Aluminum - Slope = {} GPa".format(z3.round(2))
 
     stress_unit = "GPa"
 
+    # Set axis titles, graph title, and legend
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
     ax.set_title("Stresss vs Strain", fontweight ='bold')
     ax.legend([m1, m2, m3], loc='lower right', fontsize=9)
 
+### MILD STEEL NOT DONE ###
 def mild_steel():
     s2 = np.genfromtxt("s2.csv", delimiter=",", autostrip=True)
     s2m = np.genfromtxt("s2.csv", delimiter=",", autostrip=True, usecols=range(1,378))
@@ -128,29 +140,39 @@ def mild_steel():
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
     ax.set_title("Stresss vs Strain - Galvanized Mild Steel", fontweight ='bold')
 
+
 def high_carbon():
+    # Import CSV for metal
     s1 = np.genfromtxt("s1.csv", delimiter=",", autostrip=True)
     xeng, yeng = s1[0], s1[1]
+
+    # Plot Engineering Stress Strain
     ax.plot(xeng, yeng, color='red', linewidth=1)
 
-    xtrue = [math.log(1 + i) for i in xeng] # Strain = ln(1 + eng strain)
-    ytrue = [math.prod([yeng[i], (1 + xeng[i])]) for i in range(len(yeng))] # Stress = Eng Stress(1 + eng strain)
+    # Calculate True Stress Strain
 
+    # Strain = ln(1 + Eng Strain)
+    xtrue = [math.log(1 + i) for i in xeng]
+
+    # Stress = Eng Stress(1 + Eng Strain)
+    ytrue = [math.prod([yeng[i], (1 + xeng[i])]) for i in range(len(yeng))] 
+
+    # Plot True Stress Strain
     ax.plot(xtrue, ytrue, color='#2a9d8f', linewidth=1)
     
     stress_unit = "GPa"
     
+    # Name each curve
     m1 = "Engineering Stress Strain"
     m2 = "True Stres Strain"
-
+    
+    # Set axis titles, graph title, and legend
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
     ax.set_title("Engineering vs True Stress Strain - High Carbon Steel", fontweight ='bold')
     ax.legend([m1, m2], loc='best')
 
-
-high_carbon()
-graph_common()
+mild_steel()
 plt.show()
 
 
