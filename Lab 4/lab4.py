6a0c1efe414d57156a78c551e65105c9a5a0af06
T Parker <98612931+tahayparker@users.noreply.github.com>
2023-11-16 17:36:07 +0400

    update

--- Lab 4/lab4.py
+++ Lab 4/lab4.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import shapely.geometry as sg
 import math
@@ -34,11 +35,9 @@ def metals():
     # Set axis titles, graph title, and legend
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
-    ax.set_title("Stresss vs Strain - Metals", fontweight ='bold')
+    ax.set_title("Stresss vs Strain - Metallic Samples", fontweight ='bold')
     ax.legend([m1, m2, m3], loc='best')
     plt.savefig("ENGG103 Lab 4 Part 1.png", format='png', dpi=3000, bbox_inches='tight')
-    fig.clear()
-    plt.close(fig)
 
 def pla():
     fig, ax = plt.subplots()
@@ -66,9 +65,9 @@ def pla():
     m3 = "PLA - Mixed Orientations"
 
     # Print total elongation
-    print(max(s4[0]))
-    print(max(s5[0]))
-    print(max(s6[0]))
+    print("Total elongation PLA 1 :", max(s4[0]))
+    print("Total elongation PLA 2 :", max(s5[0]))
+    print("Total elongation PLA 3 :", max(s6[0]))
 
     stress_unit = "GPa"
 
@@ -78,8 +77,6 @@ def pla():
     ax.set_title("Stresss vs Strain - PLA Samples", fontweight ='bold')
     ax.legend([m1, m2, m3], loc='best')
     plt.savefig("ENGG103 Lab 4 Part 2.png", format='png', dpi=3000, bbox_inches='tight')
-    fig.clear()
-    plt.close(fig)
 
 def metal_elastic():
     fig, ax = plt.subplots()
@@ -107,9 +104,9 @@ def metal_elastic():
     ax.plot(x3, y3, color='#fb8500', linewidth=1)
 
     # Print slope of each line for use in table
-    print(linregress(x1, y1).slope)
-    print(linregress(x2, y2).slope)
-    print(linregress(x3, y3).slope)
+    print("Metal 1 slope:", linregress(x1, y1).slope)
+    print("Metal 2 slope:", linregress(x2, y2).slope)
+    print("Metal 3 slope:", linregress(x3, y3).slope)
 
     # Get slope of each line using linregress for use in graph
     z1 = (linregress(x1, y1).slope) / 1000
@@ -126,11 +123,9 @@ def metal_elastic():
     # Set axis titles, graph title, and legend
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
-    ax.set_title("Stresss vs Strain", fontweight ='bold')
+    ax.set_title("Stresss vs Strain - Linear Region of Metallic Samples", fontweight ='bold')
     ax.legend([m1, m2, m3], loc='lower right', fontsize=9)
     plt.savefig("ENGG103 Lab 4 Part 3.png", format='png', dpi=3000, bbox_inches='tight')
-    fig.clear()
-    plt.close(fig)
 
 ### MILD STEEL NOT DONE ###
 def mild_steel():
@@ -142,6 +137,8 @@ def mild_steel():
     ax.grid(which='major', color='white', linewidth=1.2)
     ax.grid(which='minor', color='white', linewidth=0.6)
     ax.minorticks_on()
+
+    fig.set_size_inches(22, 11)
 
     s2 = np.genfromtxt("s2.csv", delimiter=",", autostrip=True)
     s2m = np.genfromtxt("s2.csv", delimiter=",", autostrip=True, usecols=range(1,378))
@@ -168,21 +165,37 @@ def mild_steel():
     plt.plot(intersection.x, intersection.y, 'x', color='orange', markersize=6)
 
     # Point the intersection of the two lines with an arrow and label it away from the intersection below the graph so that it is readable
-    ax.annotate('Intersection', xy=(intersection.x, intersection.y), xytext=(intersection.x + 0.005, intersection.y - 50), arrowprops=dict(arrowstyle='->', facecolor='black'),)
+    ax.annotate('0.2% Proof Yield Strength - {} GPa'.format(intersection.y.round(2)), xy=(intersection.x, intersection.y), xytext=(intersection.x + 0.005, intersection.y - 50), arrowprops=dict(arrowstyle='->', facecolor='black'),)
 
     # Find Ultimate Tensile Strength
-
     max_stress = max(s2[1])
     print("Max Stress =", max_stress)
 
+    # Mark Ultimate Tensile Strength
+    plt.plot(0.00497636363636364, max_stress, 'x', color='red', markersize=6)
+
+    max_strain = max(s2[0])
+    print("Max Strain =", max_strain)
+
+    ax.annotate('Ultimate Tensile Strength - {} GPa'.format(max_stress.round(2)), xy=(0.00497636363636364, max_stress), xytext=(0.00497636363636364 + 0.005, max_stress + 10), arrowprops=dict(arrowstyle='->', facecolor='black'),)
+
+    # Plot the Elastic Strain line
+    arrow = patches.FancyArrowPatch((0,-0.1), (0.0055,-0.1), color='red', arrowstyle='<->', mutation_scale=15)
+    plt.gca().add_patch(arrow)
+    plt.text(0.0025, -0.5, "$\\epsilon_E$")
+
+    # Plot the Plastic Strain Line
+    arrow = patches.FancyArrowPatch((0.0055, -0.1), (max_strain,-0.1), color='orange', arrowstyle='<->', mutation_scale=15)
+    plt.gca().add_patch(arrow)
+    plt.text(0.1626, -0.5, "$\\epsilon_P$")
     stress_unit = "GPa"
 
     ax.set_xlabel('Strain ($\\epsilon$)', fontweight ='bold')
     ax.set_ylabel('Stress ($\\sigma$, {})'.format(stress_unit), fontweight ='bold')
     ax.set_title("Stresss vs Strain - Galvanized Mild Steel", fontweight ='bold')
+
+    plt.show()
     plt.savefig("ENGG103 Lab 4 Part 4.png", format='png', dpi=3000, bbox_inches='tight')
-    fig.clear()
-    plt.close(fig)
 
 def high_carbon():
     fig, ax = plt.subplots()
@@ -224,10 +237,10 @@ def high_carbon():
     ax.set_title("Engineering vs True Stress Strain - High Carbon Steel", fontweight ='bold')
     ax.legend([m1, m2], loc='best')
     plt.savefig("ENGG103 Lab 4 Part 5.png", format='png', dpi=3000, bbox_inches='tight')
-    fig.clear()
-    plt.close(fig)
 
-metals()
-pla()
-metal_elastic()
-high_carbon()
+ch = int(input("Enter option: "))
+metals() if ch == 1 else print()
+pla() if ch == 2 else print()
+metal_elastic() if ch == 3 else print()
+mild_steel() if ch == 4 else print()
+high_carbon() if ch == 5 else print()
